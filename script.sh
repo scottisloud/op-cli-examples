a9fff703b0105b25f2f7a312f3ebb17a1143fdfb
Scott Lougheed <scott@scottlougheed.com>
2021-05-14 00:16:08 -0700

    having functioning while export
    while loop based export now functions

--- script.sh
+++ script.sh
@@ -9,9 +9,32 @@
 # function_test
 
 opusers=$(op list users --cache | jq --raw-output '.[].uuid')
-opusernames=$(op list users --cache | jq --raw-output '.[].name')
 
-	
+
+whileExport(){
+	while read -r line
+	do
+		userName=$(op get user $line | jq --raw-output '.name')  # gets user's name based on UUID
+		echo 'USER:' $userName >> test.csv
+		
+		# GROUPS - For each group the person is a member of, append it to a variable separated by a comma, then append the entire string to a csv file
+		userGroups=$(op list groups --user $line | jq --raw-output '.[].name')
+		echo 'GROUPS:' ${userGroups//$'\n'/', '} >> test.csv
+		
+		# VAULTS - For each vault the person has access to, append it to a variable separated by a comma, then append the entire string to a csv file
+		userVaults=$(op list vaults --user $line | jq --raw-output '.[].name')
+		echo 'VAULTS:' ${userVaults//$'\n'/', '} >> test.csv
+	done < <(printf '%s\n' $opusers)
+}
+
+whileExport
+
+
+
+
+
+
+
 
 # This will output to a CSV formatted as:
 # user [user name]
@@ -20,52 +43,22 @@ opusernames=$(op list users --cache | jq --raw-output '.[].name')
 # user [user name]
 # groups [group1,group2....]
 # vaults [vault1,vault2...]
-simpleExport(){
-
-	for user in $opusers
-		do
-			userName=$(op get user $user --cache | jq --raw-output '.name')  # gets user's name based on UUID
-			echo 'user', $userName >> test.csv
-			
-			# GROUPS - For each group the person is a member of, append it to a variable separated by a comma, then append the entire string to a csv file
-			userGroups=$(op list groups --user $user --cache | jq --raw-output '"\(.[].name),"')
-			echo 'groups', ${userGroups/'" "'/','} >> test.csv
-		
-			# VAULTS - For each vault the person has access to, append it to a variable separated by a comma, then append the entire string to a csv file
-			userVaults=$(op list vaults --user $user --cache | jq --raw-output '"\(.[].name),"')
-			echo 'vaults', ${userVaults/'" "'/'","'} >> test.csv	
-		done
-
-}
-
-
-whileExport(){
-	while IFS='' read -r line
-	do
-		userName=$(op get user $line --cache | jq --raw-output '.name')  # gets user's name based on UUID
-		echo 'user', $userName >> test.csv
-		
-		# GROUPS - For each group the person is a member of, append it to a variable separated by a comma, then append the entire string to a csv file
-		userGroups=$(op list groups --user $line --cache | jq --raw-output '"\(.[].name),"')
-		echo 'groups', ${userGroups/'" "'/','} >> test.csv
-		
-		# VAULTS - For each vault the person has access to, append it to a variable separated by a comma, then append the entire string to a csv file
-		userVaults=$(op list vaults --user $line --cache | jq --raw-output '"\(.[].name),"')
-		echo 'vaults', ${userVaults/'" "'/'","'} >> test.csv
-	done < <(echo $opusers)
-}
-
-whileExport
-
-
-# testVariable="one
-# two
-# three"
+# simpleExport(){
 # 
+# 	for user in $opusers
+# 		do
+# 			userName=$(op get user $user --cache | jq --raw-output '.name')  # gets user's name based on UUID
+# 			echo 'user', $userName,  >> test.csv
+# 			
+# 			# GROUPS - For each group the person is a member of, append it to a variable separated by a comma, then append the entire string to a csv file
+# 			userGroups=$(op list groups --user $user --cache | jq --raw-output '"\(.[].name),"')
+# 			echo 'groups', ${userGroups/'" "'/','} >> test.csv
+# 		
+# 			# VAULTS - For each vault the person has access to, append it to a variable separated by a comma, then append the entire string to a csv file
+# 			userVaults=$(op list vaults --user $user --cache | jq --raw-output '"\(.[].name),"')
+# 			echo 'vaults', ${userVaults/'" "'/'","'} >> test.csv	
+# 		done
 # 
-# 
-# while read -r line 
-# 	do
-# 		echo $line
-# 	done < <(echo $opusers)
-# simpleExport
+# }
+
+
