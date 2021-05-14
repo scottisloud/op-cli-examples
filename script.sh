e1aa333b80feb6489628039d4134d6b486027215
Scott Lougheed <scott@scottlougheed.com>
2021-05-13 23:23:38 -0700

    creating new while-based export
    using a while loop rather than for loop to iterate through data

--- script.sh
+++ script.sh
@@ -21,6 +21,7 @@ opusernames=$(op list users --cache | jq --raw-output '.[].name')
 # groups [group1,group2....]
 # vaults [vault1,vault2...]
 simpleExport(){
+
 	for user in $opusers
 		do
 			userName=$(op get user $user --cache | jq --raw-output '.name')  # gets user's name based on UUID
@@ -37,4 +38,34 @@ simpleExport(){
 
 }
 
-simpleExport
+
+whileExport(){
+	while IFS='' read -r line
+	do
+		userName=$(op get user $line --cache | jq --raw-output '.name')  # gets user's name based on UUID
+		echo 'user', $userName >> test.csv
+		
+		# GROUPS - For each group the person is a member of, append it to a variable separated by a comma, then append the entire string to a csv file
+		userGroups=$(op list groups --user $line --cache | jq --raw-output '"\(.[].name),"')
+		echo 'groups', ${userGroups/'" "'/','} >> test.csv
+		
+		# VAULTS - For each vault the person has access to, append it to a variable separated by a comma, then append the entire string to a csv file
+		userVaults=$(op list vaults --user $line --cache | jq --raw-output '"\(.[].name),"')
+		echo 'vaults', ${userVaults/'" "'/'","'} >> test.csv
+	done < <(echo $opusers)
+}
+
+whileExport
+
+
+# testVariable="one
+# two
+# three"
+# 
+# 
+# 
+# while read -r line 
+# 	do
+# 		echo $line
+# 	done < <(echo $opusers)
+# simpleExport
