a69ec4fdbdd11992006521c8bbbc0f9902b953e9
Scott Lougheed <scott.lougheed@agilebits.com>
2021-12-06 23:06:17 +0000

    inital attempt to bulk-update a field

--- item-update.sh
+++ item-update.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+vaultUUID='pkkyevap5rwsscvulp56zlxcqq'
+newURL='https://bluemountainsit.com/holidays/index.php'
+myItem='comkpot2qvbiox47o6bjvthkeu'
+
+# Get all UUID's into a variable
+itemUUID=($(op item list --vault $vaultUUID --format=json | jq --raw-output '.[].id'))
+
+#Loop through and add URL - CURRENTLY ADDS A NEW SECTION INSTEAD OF MODIFYING
+# for item in $itemUUID 
+# do
+# 	op item edit $item \ website=https://bluemountainsit.com/holidays/index.php --dry-run
+# done
+
+
+for item in $itemUUID
+do
+	op item edit $item url[delete] --dry-run
+done
