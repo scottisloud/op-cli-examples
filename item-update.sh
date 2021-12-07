56aa5d2ff30d7e99d8b9257e74d349dde1d7c815
Scott Lougheed <scott.lougheed@agilebits.com>
2021-12-07 00:22:34 +0000

    sanitizing values

--- item-update.sh
+++ item-update.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
+#THIS SCRIPT UPDATES OR ADDS THE VALUE OF THE WEBSITE FIELD FOR ALL ITEMS IN A SPECIFIED VAULT
 
-vaultUUID='pkkyevap5rwsscvulp56zlxcqq'
-newURL='https://bluemountainsit.com/holidays/index.php'
-myItem='comkpot2qvbiox47o6bjvthkeu'
+vaultUUID=<some vault UUID>
+newURL='https://example.com'
 
 # Get all UUID's into a variable
 itemUUID=($(op item list --vault $vaultUUID --format=json | jq --raw-output '.[].id'))
 
-#Loop through and add URL - CURRENTLY ADDS A NEW SECTION INSTEAD OF MODIFYING
-# for item in $itemUUID 
-# do
-# 	op item edit $item \ website=https://bluemountainsit.com/holidays/index.php --dry-run
-# done
-
-
+#Loop through and add URL
 for item in $itemUUID
 do
-	op item edit $item url[delete] --dry-run
+	op item edit $item website=$newURL
 done
