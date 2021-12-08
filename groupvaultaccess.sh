94b05e344c4a976b68799e65c8f37447e5ce370d
Scott Lougheed <scott.lougheed@agilebits.com>
2021-12-08 19:45:36 +0000

    minor whitespace fixes

--- groupvaultaccess.sh
+++ groupvaultaccess.sh
@@ -6,11 +6,11 @@ opGroups=($(op list groups --cache | jq -r '.[].uuid'))
 # List each group and every vault that group has access to
 
 for group in $opGroups
-do
-	groupName=$(op get group $group | jq -r '.name')
-	echo 'GROUP: ' $groupName
-	
-	groupVaults=$(op list vaults --group $group | jq -r '.[].name')
-	echo 'VAULTS: ' $groupVaults "\n\n"
-	
-done
+	do
+		groupName=$(op get group $group | jq -r '.name')
+		echo 'GROUP: ' $groupName
+		
+		groupVaults=$(op list vaults --group $group | jq -r '.[].name')
+		echo 'VAULTS: ' $groupVaults "\n\n"
+		
+	done
