da7cb1c8390dc3104eb232d094264b70b7b98378
Scott Lougheed <scott.lougheed@agilebits.com>
2021-12-08 19:44:03 +0000

    refining loop

--- groupvaultaccess.sh
+++ groupvaultaccess.sh
@@ -3,20 +3,16 @@
 
 
 # Get a list of all group UUIDs
-opGroups=$(op list groups --cache | jq -r '.[].uuid')
+opGroups=($(op1 list groups --cache | jq -r '.[].uuid'))
 
 # List each group and every vault that group has access to
-groupVaultAccess(){
-	while read -r line 
-	do
-		groupName=$(op get group $line | jq -r '.name')
-		echo 'GROUP: ' $groupName
-		
-		groupVaults=$(op list vaults --group $line | jq -r '.[].name')
-		echo 'VAULTS: ' $groupVaults "\n\n"
-		
-	done < <(printf '%s\n' $opGroups)
-}
 
-
-groupVaultAccess
+for group in $opGroups
+do
+	groupName=$(op1 get group $group | jq -r '.name')
+	echo 'GROUP: ' $groupName
+	
+	groupVaults=$(op1 list vaults --group $group | jq -r '.[].name')
+	echo 'VAULTS: ' $groupVaults "\n\n"
+	
+done
