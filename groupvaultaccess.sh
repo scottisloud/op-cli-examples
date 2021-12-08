8e06d6d94607ee8e7d8a14b1767fd907a0ee1175
Scott Lougheed <scott.lougheed@agilebits.com>
2021-12-08 19:44:27 +0000

    fixing script to work with cli v1

--- groupvaultaccess.sh
+++ groupvaultaccess.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-
-
 # Get a list of all group UUIDs
-opGroups=($(op1 list groups --cache | jq -r '.[].uuid'))
+opGroups=($(op list groups --cache | jq -r '.[].uuid'))
 
 # List each group and every vault that group has access to
 
 for group in $opGroups
 do
-	groupName=$(op1 get group $group | jq -r '.name')
+	groupName=$(op get group $group | jq -r '.name')
 	echo 'GROUP: ' $groupName
 	
-	groupVaults=$(op1 list vaults --group $group | jq -r '.[].name')
+	groupVaults=$(op list vaults --group $group | jq -r '.[].name')
 	echo 'VAULTS: ' $groupVaults "\n\n"
 	
 done
